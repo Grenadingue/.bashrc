cec5f351d853ae516c4474f35c6ee4efc340374e
Nicolas Chauvin <chauvin.nico@gmail.com>
2017-04-28 17:37:25 +0200

    Handle $SESSION_TYPE for sudo use

--- .baluchon.d/session-detection.bash
+++ .baluchon.d/session-detection.bash
@@ -1,12 +1,18 @@
-SESSION_TYPE="local"
+# If $SESSION_TYPE not set
+if [ -z ${SESSION_TYPE+x} ]; then
+    SESSION_TYPE="local"
 
-# Source: unix.stackexchange.com/a/9607
-# Detect ssh session
-if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-  SESSION_TYPE="remote/ssh"
-# many other tests omitted
-else
-  case $(ps -o comm= -p $PPID) in
-      sshd|*/sshd) SESSION_TYPE="remote/ssh";;
-  esac
+    # Source: unix.stackexchange.com/a/9607
+    # Detect ssh session
+    if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+	SESSION_TYPE="remote/ssh"
+	# many other tests omitted
+    else
+	case $(ps -o comm= -p $PPID) in
+	    sshd|*/sshd) SESSION_TYPE="remote/ssh";;
+	esac
+    fi
 fi
+
+# export SESSION_TYPE environment variable for sudo
+alias sudo='sudo SESSION_TYPE=$SESSION_TYPE'
